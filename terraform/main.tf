31c936e82580de59fb3b3551edb0e29f06a92630
Shravya-Devtools <shravyam@devtools.in>
2025-08-12 12:05:50 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "eu-central-1"  # Replace with your AWS region
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Action": "sts:AssumeRole",
+    "Principal": {
+      "Service": "lambda.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
+  }]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  roles      = [aws_iam_role.lambda_role.name]
+}
+
+resource "aws_lambda_function" "my_lambda" {
+  filename         = "lambda-package.zip"
+  function_name    = "my_lambda_function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "handler.lambda_handler"
+  runtime          = "nodejs14.x"
+  source_code_hash = filebase64sha256("lambda-package.zip")
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my_cluster"
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.docker_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnets
+    security_groups  = var.security_groups
+    assign_public_ip = true
+  }
+}
