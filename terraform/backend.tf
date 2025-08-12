922e63c50f692b9c9b4beb1364ac4b53f8c4b640
Shravya-Devtools <shravyam@devtools.in>
2025-08-12 16:16:32 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "new123456"      # Replace with your actual S3 bucket
     key    = "project/terraform.tfstate"
-    region = "eu-central-1"              # Replace with your AWS region
+    region = "ap-south-1"              # Replace with your AWS region
   }
 }
